6fc08e1a600a5dc4f5ff16ada68167078657e6be
luka <lukabarbakadze18@gmail.com>
2023-04-24 22:50:02 +0400

    import error fixed

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,6 +1,6 @@
 import EmptyState from "../components/EmptyState";
 import getCurrentUser from "../actions/getCurrentUser";
-import getFavoriteListings from "../actions/getFavoriteListing";
+import getFavoriteListings from "../actions/getFavoriteListings";
 import FavoritesClient from "./FavoritesClient";
 
 type Props = {}
