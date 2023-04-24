928daf7114d3fe5bcb3b5d9c5c25e92b948d3796
luka <lukabarbakadze18@gmail.com>
2023-04-24 17:08:36 +0400

    updated searchbar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,11 +1,52 @@
 'use client';
+import useCountries from '@/app/hooks/useCountries';
 import useSearchModal from '@/app/hooks/useSearchModal';
+import { differenceInDays } from 'date-fns';
+import { difference } from 'next/dist/build/utils';
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
 import {BiSearch} from 'react-icons/bi'
 
 type Props = {}
 
 const Search = ({}: Props) => {
-  const {onOpen} = useSearchModal();
+  const { onOpen } = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get('locationValue');
+  const startDate = params?.get('startDate');
+  const endDate = params?.get('endDate');
+  const guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if(locationValue) {
+        return getByValue(locationValue as string)?.label
+    };
+
+    return 'Anywhere';
+  }, [locationValue, getByValue]);
+
+  const durationLabel = useMemo(() => {
+    if(startDate && endDate) {
+        const start = new Date(startDate as string);
+        const end = new Date(endDate as string);
+        let diff = differenceInDays(end, start);
+
+        if(diff === 0) {
+            diff = 1;
+        }; 
+        return `${diff} Days`;
+    }
+    return 'Any Week';
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if(guestCount) {
+        return `${guestCount} Guests`;
+    }
+    return 'Add Guests';
+  }, [guestCount]);
 
   return (
     <div 
@@ -13,14 +54,14 @@ const Search = ({}: Props) => {
     className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
         <div className="flex flex-row items-center justify-between">
             <div className="text-sm font-semibold px-6">
-                Anywhere
+                {locationLabel}
             </div>
             <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-                Any week
+                {durationLabel}
             </div>
             <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
                 <div className="hidden sm:block">
-                    Add Guests
+                    {guestLabel}
                 </div>
                 <div className="p-2 bg-rose-500 rounded-full text-white">
                     <BiSearch size={18} />
